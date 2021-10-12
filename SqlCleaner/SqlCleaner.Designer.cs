48324dbeb6977b1ed203f20b13815c543ed30938
Sean <Sean@SEAN-PC>
2021-10-12 18:44:21 +1100

    Add program and icon

--- SqlCleaner/SqlCleaner.Designer.cs
+++ SqlCleaner/SqlCleaner.Designer.cs
@@ -29,20 +29,60 @@ namespace SqlCleaner
         /// </summary>
         private void InitializeComponent()
         {
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.cleanBtn = new System.Windows.Forms.Button();
+            this.copyBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(12, 12);
+            this.inputText.Multiline = true;
+            this.inputText.Name = "inputText";
+            this.inputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputText.Size = new System.Drawing.Size(750, 368);
+            this.inputText.TabIndex = 0;
+            // 
+            // cleanBtn
+            // 
+            this.cleanBtn.Location = new System.Drawing.Point(12, 386);
+            this.cleanBtn.Name = "cleanBtn";
+            this.cleanBtn.Size = new System.Drawing.Size(370, 23);
+            this.cleanBtn.TabIndex = 1;
+            this.cleanBtn.Text = "Clean";
+            this.cleanBtn.UseVisualStyleBackColor = true;
+            this.cleanBtn.Click += new System.EventHandler(this.cleanBtn_Click);
+            // 
+            // copyBtn
+            // 
+            this.copyBtn.Location = new System.Drawing.Point(392, 386);
+            this.copyBtn.Name = "copyBtn";
+            this.copyBtn.Size = new System.Drawing.Size(370, 23);
+            this.copyBtn.TabIndex = 2;
+            this.copyBtn.Text = "Copy";
+            this.copyBtn.UseVisualStyleBackColor = true;
+            this.copyBtn.Click += new System.EventHandler(this.copyBtn_Click);
             // 
             // SqlCleaner
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(777, 421);
+            this.Controls.Add(this.copyBtn);
+            this.Controls.Add(this.cleanBtn);
+            this.Controls.Add(this.inputText);
             this.Name = "SqlCleaner";
-            this.Text = "SqlCleaner";
+            this.Text = "SqlCleaner (sp_prepexec remover)";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox inputText;
+        private System.Windows.Forms.Button cleanBtn;
+        private System.Windows.Forms.Button copyBtn;
     }
 }
 
--- SqlCleaner/SqlCleaner.cs
+++ SqlCleaner/SqlCleaner.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +17,57 @@ namespace SqlCleaner
         public SqlCleaner()
         {
             InitializeComponent();
+            Icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetExecutingAssembly().Location);
         }
 
+        private void cleanBtn_Click(object sender, EventArgs e)
+        {
+            TSqlParser parser = new TSql120Parser(true);
+            IList<ParseError> parseErrors;
+            TSqlFragment sqlFragment = parser.Parse(new StringReader(inputText.Text), out parseErrors);
+
+            TSqlScript script = (TSqlScript)sqlFragment;
+            var batch = script.Batches[0];
+
+            ExecuteStatement stmt = batch.Statements.OfType<Microsoft.SqlServer.TransactSql.ScriptDom.ExecuteStatement>().First();
+
+            var paramterers = stmt.ExecuteSpecification.ExecutableEntity.Parameters;
+
+            var paramaterHeader = ((StringLiteral)paramterers[1].ParameterValue).Value;
+            var queryHeader = ((StringLiteral)paramterers[2].ParameterValue).Value;
+
+            var x = paramterers[3].ParameterValue.GetType().ToString();
+
+            var paramsArray = paramaterHeader.Split(',');
+
+            for (int i = 0; i < paramsArray.Length; i++)
+            {
+                var paramOffset = i + 3;
+                if (paramterers[paramOffset].ParameterValue.GetType().ToString() == "Microsoft.SqlServer.TransactSql.ScriptDom.StringLiteral")
+                {
+                    paramsArray[i] = paramsArray[i] + string.Format(" = '{0}',", ((StringLiteral)paramterers[paramOffset].ParameterValue).Value);
+                }
+                else if (paramterers[paramOffset].ParameterValue.GetType().ToString() == "Microsoft.SqlServer.TransactSql.ScriptDom.IntegerLiteral")
+                {
+                    paramsArray[i] = paramsArray[i] + string.Format(" = {0},", ((IntegerLiteral)paramterers[paramOffset].ParameterValue).Value);
+                }
+                else if (paramterers[paramOffset].ParameterValue.GetType().ToString() == "Microsoft.SqlServer.TransactSql.ScriptDom.NumericLiteral")
+                {
+                    paramsArray[i] = paramsArray[i] + string.Format(" = {0},", ((NumericLiteral)paramterers[paramOffset].ParameterValue).Value);
+                }
+            }
+
+            var declareStmt = "DECLARE " + string.Join("\r\n\t", paramsArray);
+            declareStmt = declareStmt.Remove(declareStmt.Length - 1) + ";";
+            var finalQuery = declareStmt + "\r\n\r\n" + queryHeader;
+
+            inputText.Text = finalQuery;
+
+        }
+
+        private void copyBtn_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(inputText.Text);
+        }
     }
 }
